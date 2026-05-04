b5d9f3b5455b1db20a0c1b07fbf13d5383236263
Pavel Krot <krotpv@gmail.com>
2026-05-04 18:02:00 +0800

    feat: better horde manipulation algorithm for dedicated servers

--- Contents/mods/PZ_Rin_HordeRush/media/lua/shared/HordeRush_SoundEvents.lua
+++ Contents/mods/PZ_Rin_HordeRush/media/lua/shared/HordeRush_SoundEvents.lua
@@ -3,8 +3,10 @@ require "HordeRush_Utils"
 
 local WorldSoundManager = getWorldSoundManager()
 
-local lastCalmIdx = 0
-local lastStormIdx = 0
+local calmLastIdx = 0
+local calmLastTick = 0
+local stormLastIdx = {}
+local stormLastTick = {}
 
 local function makeNoise(player, x, y, radius, volume)
     if player then
@@ -12,32 +14,57 @@ local function makeNoise(player, x, y, radius, volume)
     end
 end
 
-local function makeGatherNoise(player, targetX, targetY, hordeDistance, volume, lastIdx)
+local function makeGatherNoise(player, targetX, targetY, hordeDistance, volume, lastIdx, lastTick, waitTicks)
+    lastTick = lastTick + 1
+    if lastTick < waitTicks then return lastIdx, lastTick end
+
     local hordeRadius = hordeDistance * 1.42
     local x1, x2, y1, y2 = RHR_MOD.GetHordeSquare(targetX, targetY, hordeDistance)
 
-    local checkIdx = (lastIdx + 1) % 4
+    local checkIdx = (lastIdx + 1) % 6
     if checkIdx == 0 then
-        makeNoise(player,x2, y1, hordeRadius, volume)
+        makeNoise(player,x1, y1, hordeRadius, volume)
     elseif checkIdx == 1 then
-        makeNoise(player,x1, y1, hordeRadius, volume - 500)
+        makeNoise(player,x2, y2, hordeRadius, volume)
     elseif checkIdx == 2 then
-        makeNoise(player,x2, y2, hordeRadius, volume - 1000)
+        -- do nothing
     elseif checkIdx == 3 then
-        makeNoise(player,x1, y2, hordeRadius, volume - 1500)
+        makeNoise(player,x1, y2, hordeRadius, volume)
+    elseif checkIdx == 4 then
+        makeNoise(player,x2, y1, hordeRadius, volume)
+    elseif checkIdx == 5 then
+        -- do nothing
     end
-    return checkIdx
+
+    lastTick = 0
+    return checkIdx, lastTick
 end
 
 function RHR_MOD.CalmPhaseEventNoise(player, targetX, targetY, hordeDistance)
-    lastCalmIdx = makeGatherNoise(player, targetX, targetY, hordeDistance, 5000, lastCalmIdx)
+    calmLastIdx, calmLastTick = makeGatherNoise(player, targetX, targetY, hordeDistance, 5000, calmLastIdx, calmLastTick, 5)
 end
 
 function RHR_MOD.StormPhaseEventNoise(player, targetX, targetY, offset, hordeDistance)
-    lastStormIdx = makeGatherNoise(player, targetX, targetY, 110, 1000, lastStormIdx)
+    local idx = 0
+    local volume = 1000
+    local currDistance = hordeDistance
+    while currDistance > 0 do
+        if not stormLastIdx[idx] then stormLastIdx[idx] = 0 end
+        if not stormLastTick[idx] then stormLastTick[idx] = 0 end
+
+        if currDistance < 110 then
+            stormLastIdx[idx], stormLastTick[idx] = makeGatherNoise(player, targetX, targetY, 105, volume, stormLastIdx[idx], stormLastTick[idx], 0)
+            break
+        else
+            stormLastIdx[idx], stormLastTick[idx] = makeGatherNoise(player, targetX, targetY, currDistance, volume, stormLastIdx[idx], stormLastTick[idx], 0)
+            currDistance = currDistance - 100
+            volume = volume + 1000
+            idx = idx + 1
+        end
+    end
 
     local offsetX, offsetY = ZombRandBetween(-offset, offset), ZombRandBetween(-offset, offset)
     local x, y = targetX + offsetX, targetY + offsetY
-    makeNoise(player, x, y, hordeDistance * 2, 5000)
+    makeNoise(player, x, y, hordeDistance * 2, volume + 1000)
 end
 
